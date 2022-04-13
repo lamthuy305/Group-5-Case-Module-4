ad6fbcb19e5fba256b30c9ca0e542a4ec2301a5e
Your Name <princetinkie97@gmail.com>
2022-04-13 19:17:40 +0700

    them categoryController

--- src/main/java/com/example/casemodule4group5/config/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/casemodule4group5/config/securityConfig/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/",
                         "/**",
                         "/login",
-                        "/register","/image/**").permitAll()
+                        "/register","/image/**","/categories/**").permitAll()
                 .antMatchers("/foods/**")
                 .access("hasRole('ROLE_ADMIN')")
                 .anyRequest().authenticated()
